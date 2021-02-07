0e10d162cc58dbdb545fa235a52db02d0cba4fd4
Md Zia Hossain <mdziahossain@yandex.com>
2021-02-07 19:06:33 +0600

    Milestone Project-05 v2.1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function loadData() {
   fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")  
   .then(res => res.json())
   .then(data => { 
-    // console.log(data);
+    console.log(data);
     const meals = data.meals;
     displayMeals(meals)
   });
@@ -22,7 +22,7 @@ function displayMeals(data) {
       const mealInfo = `
       <img class="meal-img" src="${meal.strMealThumb}" />
       <h5 class="meal-name">${meal.strMeal}</h5>
-      <button onclick="mealDetails()">Details</button>
+      <button class="mealBtn" onclick="mealDetails('${meal.strIngredient1}')">Instruction</button>
       `;
       
       mealDiv.innerHTML = mealInfo;
@@ -31,7 +31,23 @@ function displayMeals(data) {
 }
 
 const mealDetails = details => {
-  // console.log(details);
+  console.log(details);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${details}
+`;
+  console.log(url);
+  fetch(url)
+  .then(res => res.json())
+  .then(data => 
+  renderDetails(data[0].meal));
 }
 
 loadData();
+
+const renderDetails = meal => {
+  console.log(meal);
+  const mealDetails = document.getElementById("meal-details");
+  mealDetails.innerHTML = `
+    <h3>${meal.strMeal}</h3>
+    <p>${meal.strInstructions}</p>
+  `
+}
